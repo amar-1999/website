82dbb8073e60f8e31fb8432bc6bf234ff335f2b9
Damarakeswar Guddanti <damarakeswarg1999@gmail.com>
2021-07-30 22:38:03 +0530

    made changes

--- mail/contact.php
+++ mail/contact.php
@@ -13,8 +13,11 @@ $to = "damarakeswarg1999@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
-$header .= "Reply-To: $email";	
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+//$header .= "Reply-To: $email";	
+if($email!=NUll){
+  mail($to, $subject, $body, $header);
+}
+//if(!mail($to, $subject, $body, $header))
+//   http_response_code(500);
+header("Location: thankyou.html");
 ?>
